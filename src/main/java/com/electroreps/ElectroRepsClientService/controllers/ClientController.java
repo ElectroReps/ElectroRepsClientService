f72e9ede7107397f621b45f6437b0b0f0bf14683
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2025-05-27 11:22:29 +0200

    Client controller commit

--- src/main/java/com/electroreps/ElectroRepsClientService/controllers/ClientController.java
+++ src/main/java/com/electroreps/ElectroRepsClientService/controllers/ClientController.java
@@ -1,4 +1,45 @@
 package com.electroreps.ElectroRepsClientService.controllers;
 
+import com.electroreps.ElectroRepsClientService.dtos.ClientInfoDto;
+import com.electroreps.ElectroRepsClientService.models.Client;
+import com.electroreps.ElectroRepsClientService.repositories.ClientRepository;
+import com.electroreps.ElectroRepsClientService.services.ClientService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clients")
 public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping("/{clientId}")
+    public ResponseEntity<?> getClientById(@PathVariable Long clientId) {
+        return clientService.getClientById(clientId);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getClientWithParams(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        return clientService.getClientWithParams(name, email);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createClient(@RequestBody @Valid Client client) {
+        return clientService.createClient(client);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> updateClient(@RequestParam Long id, @RequestBody ClientInfoDto client) {
+        return clientService.updateClient(id, client);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteClient(@RequestParam Long id) {
+        return clientService.deleteClient(id);
+    }
 }
